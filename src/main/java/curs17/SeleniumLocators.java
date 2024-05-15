cc7943dc7d9e75cf15a1884d553ba125b78c078e
Dragos Tanta <dragos.tanta@gmail.com>
2024-05-15 20:39:30 +0300

    curs17 locatori

---
+++ src/main/java/curs17/SeleniumLocators.java
@@ -0,0 +1,91 @@
+package curs17;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+import selenium.utils.BaseTest;
+
+public class SeleniumLocators extends BaseTest{
+	
+	/*
+	 * tagname
+	 * linkText
+	 * partialLinkText
+	 * class
+	 * id
+	 * name
+	 * css Selector
+	 * Xpath
+	 * 
+	 */
+	
+	/*
+	 * Relative locators:
+	 * toLeftOf()
+	 * toRightof()
+	 * above()
+	 * below()
+	 * near()
+	 */
+	
+	@Test(priority=1)
+	public void tagnameLocator() {
+		//<em>Discover</em>
+		
+		WebElement discoverText = browser.findElement(By.tagName("em"));
+		String text =discoverText.getText();//returneaza textul dintre tag-urile HTML	
+		System.out.println(text);
+		assertEquals(text, "Discover");
+	}
+	
+	@Test(priority=2)
+	public void linkTextLocator() {
+		//<a href="https://keybooks.ro/shop/">Books</a>
+		
+		WebElement shopLink = browser.findElement(By.linkText("BOOKS"));
+		shopLink.click();	
+		assertEquals(browser.getCurrentUrl(), "https://keybooks.ro/shop/");
+		
+	}
+	
+	@Test(priority=3)
+	public void partialLinkTextLocator() {
+		//<a href="https://keybooks.ro/shop/a-banquet-of-mouse/">A banquet of  mouse</a>
+		WebElement book = browser.findElement(By.partialLinkText("banquet"));
+		book.click();
+		assertEquals(browser.getCurrentUrl(), "https://keybooks.ro/shop/a-banquet-of-mouse/");
+	}
+	
+	@Test(priority=4)
+	public void classNameLocator() {
+		
+		WebElement price = browser.findElement(By.className("price"));
+		System.out.println(price.getText());
+		
+		assertTrue(price.getText().contains("9.99"));
+			
+	}
+	@Test(priority=5)
+	public void idLocator() {
+		
+		WebElement reviewTab = browser.findElement(By.id("tab-title-reviews"));
+		reviewTab.click();
+		
+		WebElement commentBox = browser.findElement(By.id("comment")); 
+		assertTrue(commentBox.isDisplayed());
+	}
+	
+	/*
+	 * isDisplayed() --> verifica daca un element este vizibil
+	 * isEnabled()--> verifica daca un element este enabled
+	 * isSelected()--> doar pentru checkbox sau radio button
+	 * 
+	 */
+	
+	
+
+}
